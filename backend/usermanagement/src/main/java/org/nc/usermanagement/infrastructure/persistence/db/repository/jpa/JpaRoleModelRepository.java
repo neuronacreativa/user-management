a421400a7610eddb2a60fc712234fbf5514d5034
neuronacreativa <ricardo.ortega.avalo@gmail.com>
2022-01-24 00:55:54 +0100

    Added pagination to Read Role use case

--- backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/persistence/db/repository/jpa/JpaRoleModelRepository.java
+++ backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/persistence/db/repository/jpa/JpaRoleModelRepository.java
@@ -2,12 +2,13 @@ package org.nc.usermanagement.infrastructure.persistence.db.repository.jpa;
 
 import org.nc.usermanagement.infrastructure.persistence.db.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface JpaRoleModelRepository extends JpaRepository<RoleModel, String> {
+public interface JpaRoleModelRepository extends PagingAndSortingRepository<RoleModel, Integer> {
     Optional<RoleModel> findByUuid(String uuid);
     Optional<RoleModel> findByRoleName(String roleName);
     Optional<RoleModel> findByUuidAndRoleName(String uuid, String roleName);
