47a31285d10d18b348f2a3e79f6b9b9e6e3b9a76
neuronacreativa <ricardo.ortega.avalo@gmail.com>
2022-01-24 01:36:55 +0100

    roles/find endpoint in ReadController modified to GET method

--- backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/rest/controller/role/read/ReadRoleController.java
+++ backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/rest/controller/role/read/ReadRoleController.java
@@ -5,7 +5,7 @@ import graphql.GraphQL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class ReadRoleController {
     @Autowired
     private GraphQL graphQL;
 
-    @PostMapping("roles/find")
+    @GetMapping("roles/find")
     public ResponseEntity<Object> roles(@RequestBody String query) {
         ExecutionResult execute = graphQL.execute(query);
         return new ResponseEntity<>(execute, HttpStatus.OK);
