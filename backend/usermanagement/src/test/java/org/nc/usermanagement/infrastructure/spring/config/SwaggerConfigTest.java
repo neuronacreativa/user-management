32caa065a9bbb0252819b8a5996bdceccb370742
neuronacreativa <ricardo.ortega.avalo@gmail.com>
2022-01-24 23:16:23 +0100

    New Test for generate Swagger Json UserManagement API static definition

---
+++ backend/usermanagement/src/test/java/org/nc/usermanagement/infrastructure/spring/config/SwaggerConfigTest.java
@@ -0,0 +1,34 @@
+package org.nc.usermanagement.infrastructure.spring.config;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+@SpringBootTest
+class SwaggerConfigTest {
+    @Autowired
+    WebApplicationContext context;
+
+    @Test
+    public void generateSwagger() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andDo((result) ->
+                    FileUtils.writeStringToFile(
+                        new File("swagger-userManagement.json"),
+                        result.getResponse().getContentAsString(),
+                        StandardCharsets.UTF_8
+                    )
+                );
+
+    }
+}
