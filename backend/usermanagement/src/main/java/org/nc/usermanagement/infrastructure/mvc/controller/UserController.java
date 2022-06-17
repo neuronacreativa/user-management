8f8c8e0118a237bbd5988c58a106244131baf0d6
neuronacreativa <ricardo.ortega.avalo@gmail.com>
2022-06-18 00:14:26 +0200

    Refactor home method and added TODOs for next features

--- backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/mvc/controller/UserController.java
+++ backend/usermanagement/src/main/java/org/nc/usermanagement/infrastructure/mvc/controller/UserController.java
@@ -1,18 +1,18 @@
 package org.nc.usermanagement.infrastructure.mvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("backend")
 public class UserController {
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String home() {
+        // TODO: Add Users and Roles Lists
+        // TODO: and Users and Roles creation forms
+        // TODO: Add Tests
         return "home";
     }
 
